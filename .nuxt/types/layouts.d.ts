52f0a9a480bec615822f44195bcf75840471a745
Adam DeHaven <adam.dehaven@konghq.com>
2024-10-08 08:52:42 -0400

    chore: apply gitignore

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = string
-declare module "../../node_modules/.pnpm/nuxt@3.10.3_typescript@5.4.5_vite@5.1.5_vue-tsc@2.0.21/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
